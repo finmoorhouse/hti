5d08f3b66c39b876d0d20959915313e729d8dcfc
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2023-08-18 10:09:37 +0100

    Removed note, updated image shortcodes

--- shortcodes.js
+++ shortcodes.js
@@ -6,20 +6,21 @@ const matter = require("gray-matter");
 const fs = require("fs");
 
 //Maybe this is inefficient?
-async function imageShortcode(src, alt) {
+async function imageShortcode(src, alt = src, size = "medium") {
   const metadataOptions = {
-    widths: [300, 600],
+    widths: [100, 600],
     formats: ["avif", "jpeg"],
     outputDir: "build/img/",
     cacheOptions: {
       // if a remote image URL, this is the amount of time before it fetches a fresh copy
-      duration: "10d",
+      duration: "30d",
       // project-relative path to the cache directory
       directory: ".cache",
       removeUrlQueryParams: true,
     },
   };
   const str = fs.readFileSync(this.page.inputPath, "utf8");
+  // Reading the file to get the 'number' frontmatter field.
   const data = matter(str).data;
   let urlPath = LFS_URL;
   if (data.number) {
@@ -35,7 +36,6 @@ async function imageShortcode(src, alt) {
   } catch (error) {
     // Handle error
     console.error("Error retrieving LFS image:", error);
-
     // Optionally return placeholder
     metadata = await Image(
       "https://upload.wikimedia.org/wikipedia/commons/1/14/Rubber_Duck_(8374802487).jpg",
@@ -56,12 +56,10 @@ async function imageShortcode(src, alt) {
   });
 }
 
-async function featuredImageShortcode(src, alt) {
-  let sizes = "100vw";
-  let metadata = "";
-  let fullSrc = LFS_URL + `episodes/${src}?nf_resize=smartcrop&w=100&h=100`;
+async function featuredImageShortcode(content, src, alt) {
+  let fullSrc = LFS_URL + `episodes/${src}?nf_resize=smartcrop&w=1000`;
   const metadataOptions = {
-    widths: [300],
+    widths: [1000],
     formats: ["avif", "jpeg"],
     outputDir: "build/img/",
     cacheOptions: {
@@ -77,29 +75,60 @@ async function featuredImageShortcode(src, alt) {
     // New image source is host URL plus episode number plus filename.
     updatedSrc = fullSrc;
     // Docs on resizing images with Netlify LFS: https://docs.netlify.com/large-media/transform-images/
-    metadata = await Image(updatedSrc, metadataOptions);
+    returnedImage = await Image(updatedSrc, metadataOptions);
   } catch (error) {
     // Handle error
     console.error("Error retrieving LFS image:", error);
 
     // Optionally return placeholder
-    metadata = await Image(
+    returnedImage = await Image(
       "https://upload.wikimedia.org/wikipedia/commons/1/14/Rubber_Duck_(8374802487).jpg",
       metadataOptions
     );
   }
+  return `<div
+  class="p-0 w-3/12 sm:w-full bg-cover overflow-hidden aspect-square sm:aspect-video"
+  style="background-image: url('${returnedImage.avif[0].url}')"
+  >${content}
+</div>`;
+}
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
+async function backgroundImageShortcode(content, src) {
+  let fullSrc = LFS_URL + `episodes/${src}?nf_resize=smartcrop&w=1000`;
+  const metadataOptions = {
+    widths: [1000],
+    formats: ["avif", "jpeg"],
+    outputDir: "build/img/",
+    cacheOptions: {
+      // if a remote image URL, this is the amount of time before it fetches a fresh copy
+      duration: "10d",
+      // project-relative path to the cache directory
+      directory: ".cache",
+      removeUrlQueryParams: true,
+    },
   };
+  try {
+    //console.log("Downloading:", fullSrc);
+    // New image source is host URL plus episode number plus filename.
+    updatedSrc = fullSrc;
+    // Docs on resizing images with Netlify LFS: https://docs.netlify.com/large-media/transform-images/
+    returnedImage = await Image(updatedSrc, metadataOptions);
+  } catch (error) {
+    // Handle error
+    console.error("Error retrieving LFS image:", error);
 
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes, {
-    whitespaceMode: "inline",
-  });
+    // Optionally return placeholder
+    returnedImage = await Image(
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/Rubber_Duck_(8374802487).jpg",
+      metadataOptions
+    );
+  }
+  return `<div
+    class="rounded-lg ring-2 ring-slate-200 p-0 mb-8 shadow-md w-full bg-cover relative overflow-hidden  "
+    style="background-image: url('${returnedImage.avif[0].url}')"
+    ><div class="p-4 z-10 relative w-full h-full pt-10 md:pt-[130px]">${content}</div>
+    <div class="absolute inset-0 bg-gradient-to-t from-slate-800 to-transparent opacity-80 z-0 mix-blend-multiply"></div>
+  </div>`;
 }
 
 // Other shortcodes
@@ -117,32 +146,14 @@ function dateToString(date) {
   });
 }
 
-
-function note(content, label) {
-  return `<span class="sidenote"><input
-      aria-label="Show sidenote"
-      type="checkbox"
-      id="sidenote__checkbox--${label.replaceAll(" ", "-")}"
-      class="sidenote__checkbox"><label
-      tabindex="0"
-      title=""
-      aria-describedby="sidenote-${label.replaceAll(" ", "-")}"
-      for="sidenote__checkbox--${label.replaceAll(" ", "-")}"
-      class="sidenote__button"
-      >${label}</label><small
-      id="sidenote-${label.replaceAll(" ", "-")}"
-      class="sidenote__content"><span class="sidenote__content-parenthesis"
-      >(sidenote: </span>${content}<span class="sidenote__content-parenthesis">)</span></small></span>`;
-}
-
 function aside(content) {
-  return `<aside class=" rounded-md px-8 py-2">${content}</aside>`;
+  return `<aside class="rounded-md px-8 py-2 bg-slate-100">${content}</aside>`;
 }
 
 module.exports = {
   imageShortcode,
   featuredImageShortcode,
+  backgroundImageShortcode,
   dateToString,
-  note,
   aside,
 };
